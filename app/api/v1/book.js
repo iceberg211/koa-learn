04875ec6bfc0d03f3dfdef41e65dde206412c3bd
何炜 <hewei@ipin.com>
2019-08-12 16:51:44 +0800

    添加book数据结构

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,6 +1,17 @@
-const router = require('koa-router')();
+const router = require('koa-router')({
+  prefix: '/v1/book'
+});
 const { PositiveIntegerValidator } = require('../../validators');
+const { HotBook } = require('../../models/hotbook')
 
+// 图书的数据没有存放在本地，图书的基本数据是调用其他服务
+
+router.get('/hot_list', async (ctx, next) => {
+  const books = await HotBook.getAll();
+  ctx.body = {
+    books
+  }
+});
 router.get('/v1/book', (ctx, next) => {
   ctx.body = {
     book: 'v1'
