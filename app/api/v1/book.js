c54578b062e3a89c459fe91f1af61925cd7b69a4
何炜 <hewei@ipin.com>
2019-08-13 10:43:43 +0800

    完善书籍接口

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,6 +1,8 @@
 const { Book } = require('../../models/book');
 const { PositiveIntegerValidator, SearchValidator } = require('../../validators');
 const { HotBook } = require('../../models/hotbook');
+const { Auth } = require('../../../middleware/auth');
+const { Favor } = require('../../models/favor');
 const router = require('koa-router')({
   prefix: '/v1/book'
 });
@@ -15,31 +17,41 @@ router.get('/hot_list', async (ctx, next) => {
   }
 });
 
-router.get('/search', async (ctx, next) => {
+router.get('/search', new Auth().m, async (ctx, next) => {
   const v = await new SearchValidator().validate(ctx)
   const { keyword, count, start, summary } = v.get('query');
   const book = HotBook.getSearchInfo(encodeURI(keyword), count, start, summary);
   handleResult({ ctx, data: book })
 });
 
-router.get('/:id/detail', async (ctx, next) => {
+router.get('/:id/detail', new Auth().m, async (ctx, next) => {
   const v = await new PositiveIntegerValidator().validate(ctx)
   const { id } = v.get('path')
   const detail = await Book.getDetail(id)
   handleResult({ ctx, data: detail })
 })
 
-
-router.get('/v1/book', (ctx, next) => {
-  ctx.body = {
-    book: 'v1'
-  }
-});
-
-router.post('/v1/:id/books', (ctx, next) => {
+router.post('/v1/:id/books', new Auth().m, (ctx, next) => {
   const v = new PositiveIntegerValidator().validate(ctx);
   const id = v.get('path.id', parsed = false);
   ctx.body = 'succeuss'
-})
+});
+
+// 书籍点赞数量
+router.get('/favor/count', new Auth().m, (ctx, next) => {
+  const favor = await Favor.getMyFavorBookCount(ctx.auth.uid);
+  ctx.body = {
+    count: favor,
+  }
+});
+
+router.get('/:book_id/favor', new Auth().m, (ctx, next) => {
+  const v = new PositiveIntegerValidator().validate(ctx, {
+    id: 'book_id',
+  });
+  const favorState = await Favor.getBookFavorCount(ctx, auth.id, v.get('path.book_id'));
+
+  ctx.body = favorState;
+});
 
 module.exports = router;
