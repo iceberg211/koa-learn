22fb12ee007f6530acd0ce1404b0f0c63fb77b49
何炜 <187399692@qq.com>
2019-12-10 09:58:12 +0800

    feat: 添加模板引擎

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const static = require('koa-static');
 const path = require('path');
 const catchError = require('./middleware/error');
 const InitManager = require('./core/init');
+const views = require('koa-views')
+
+
 
 const app = new Koa();
 
@@ -12,16 +15,15 @@ app.use(parser());
 // 可以直接通过地址直接访问
 app.use(static(path.join(__dirname, './static')))
 //  全局的错误捕捉
-app.use(catchError); 
+app.use(catchError);
 
+app.use(views(resolve(__dirname, 'app/views'), {
+  extension: 'pug',
+  options: {
+    moment: moment
+  }
+}))
 
-// logger
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
 
 InitManager.initCore(app);
 app.listen(4200, () => console.log('listen at 4200'));
